1f86f8e4a6552790639cc325ae3e664504a37512
Eric Mangin <ericpaulmangin@gmail.com>
2023-07-03 23:17:25 -0700

    Adjusted the 'src' folder's index.js file quite a bit and moved the prior version over to 'v3_src' folder + learning quite a bit about how to work with canvas and even using some trigonometry equations (and converting radians back into milliseconds) within it.

--- src/index.js
+++ src/index.js
@@ -1,142 +1,368 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+// Working with Canvas to create a cool "star crawler" that follows the cursor around
+// and...ok, I think that I finally got it to work! Lol, stardate: Mon July 3rd, 2023
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.requestAnimFrame = function () {
+	return (
+		window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {
+			window.setTimeout(callback, 1000 / 60);
+		}
+	);
+};
 
-let particlesArray; // declare particles array
-
-// get mouse position 
-const mouse = {
-    x: null,
-    y: null,
-    radius: (canvas.height/80) * (canvas.width/80)
+/*  setup the canvas to create a 2D rendering of the canvas ('c'), so I can draw onto it, 
+        and set its boundaries equal to the width and height of the user's browser window 
+        (using 'w', and 'h')
+    */
+function init(elemid) {
+	let canvas = document.getElementById(elemid);
+	let c = canvas.getContext('2d');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let w = canvas.width;
+	let h = canvas.height;
+	c.fillStyle = 'rgba(30,30,30,1)';
+	c.fillRect(0, 0, w, h);
+	return { c: c, canvas: canvas };
 }
 
-// create particle
-class Particle {
-    constructor(x, y, directionX, directionY, size, color) {
-        this.x = x;
-        this.y = y;
-        this.directionX = directionX;
-        this.directionY = directionY;
-        this.size = size;
-        this.color = color;
-    }
+window.onload = function () {
+	/* added in 'canvasData' to call 'init()' once, and will use its returned values
+    // before, I had it setup with:
+        let c = init("canvas").c,
+        canvas = init("canvas").canvas
+    // which was duplicating the resizing and unneccessarily overriding width and height
+    */
+	let canvasData = init('canvas');
+	let c = canvasData.c,
+		canvas = canvasData.canvas,
+		w = canvas.width,
+		h = canvas.height,
+		// going to be tracking user's 'mouse' using x & y coordinates (i.e. current position)
+		mouse = { x: false, y: false },
+		// I'll use the 'last_mouse' object to track user's mouse's previous position
+		last_mouse = {};
 
-    // Updated the method to draw() particles here in the v3
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        // gradient color for the particle (neon pink to neon green)
-        let gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size);
-        gradient.addColorStop(0, 'rgba(255, 0, 255, 1)'); // neon pink
-        gradient.addColorStop(1, 'rgba(0, 255, 0, 1)'); // neon green
-        ctx.fillStyle = gradient;
-        ctx.fill();
-    }
+	// create a helper function to calc Euclidean distance using Math.sqrt & Math.pow
+	function dist(p1x, p1y, p2x, p2y) {
+		return Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
+	}
 
-    // check particle position, check mouse position, move the particle, draw the particle
-    update() {
-        // check if particle is still within canvas
-        if (this.x > canvas.width || this.x < 0) {
-            this.directionX = -this.directionX;
+	class segment {
+		// when a new instance of the segment class is created, the constructor's called
+		// l = length of the segment; and a = angle of the segment
+		constructor(parent, l, a, first) {
+			this.first = first;
+			// if first is true, this segment's the first segment, and pos is set to parent
+			if (first) {
+				this.pos = {
+					x: parent.x,
+					y: parent.y,
+				};
+				// otherwise, if first is false, pos is set to nextPos
+			} else {
+				this.pos = {
+					x: parent.nextPos.x,
+					y: parent.nextPos.y,
+				};
+			}
+
+			// nextPos is calculated using trigonometric functions to determine x,y coordinates
+			// it uses current position (pos), length (l) and angle (ang)
+			this.l = l;
+			this.ang = a;
+			this.nextPos = {
+				x: this.pos.x + this.l * Math.cos(this.ang),
+				y: this.pos.y + this.l * Math.sin(this.ang),
+			};
+		}
+		// then, I want to call the update(t) method here in the segment class
+		// this will allow the Star Crawler to be repositioned, using the
+		// position (pos) and angle (ang)of a segment, based on the target position
+		update(t) {
+			// calculate the angle the segment should move based off comparing the target (t)
+			// to the segment's current position (this.pos), using the Math.atan2() function
+			this.ang = Math.atan2(t.y - this.pos.y, t.x - this.pos.x);
+			this.pos.x = t.x + this.l * Math.cos(this.ang - Math.PI);
+			this.pos.y = t.y + this.l * Math.sin(this.ang - Math.PI);
+			this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+			this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+		}
+		// Then I'm going to call in the fallback(t) function to update the position
+		// of an object based on a target(t) within a two-dimensional coordinate system
+		// the goal here is to make the x,y coordinates accessible (this.pos.x & this.pos.y)
+		// and bring the object closer to the target position, creating a smoother transition
+		fallback(t) {
+			this.pos.x = t.x;
+			this.pos.y = t.y;
+			this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+			this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+		}
+		show() {
+			c.lineTo(this.nextPos.x, this.nextPos.y);
+		}
+	}
+
+	class crawler {
+		constructor(x, y, l, n, a) {
+			this.x = x;
+			this.y = y;
+			this.l = l;
+			this.n = n; // number of segments
+			this.t = {};
+            this.hue = Math.floor(Math.random() * (345 - 330 + 1) + 330); // for pink
+        	this.rand = Math.random();
+
+			// initialize the first segment (i.e. the lone 'this' creates the first instance
+			// of the crawler, which is essentially its first leg), where angle = 0, to start
+			// My for loop is used to fill out the segments array with each new segment instance
+			// first segment is marked as such by the 'true' parameter, and new segments shown
+			// !first by utilizing the 'false' parameter within the for loop & its push method
+			this.segments = [new segment(this, this.l / this.n, 0, true)];
+			for (let i = 1; i < this.n; i++) {
+				this.segments.push(
+					new segment(this.segments[i - 1], this.l / this.n, 0, false)
+				);
+			}
+		} // close of constructor() for Crawler class
+		move(last_target, target) {
+			// using Math.atan2() again to calculate the angle (in radians) for my cralwer movements
+			this.angle = Math.atan2(target.y - this.y, target.x - this.x);
+
+			// calculate duration (dt) based on the Euclidean distance (dist function) of the
+			// last target position and the current target position, then add 5 units in order
+			// to gain a slight overlap between current position and the new target position
+			this.dt = dist(last_target.x, last_target.y, target.x, target.y) + 5;
+
+			// then subtract 80% (of the duration multiplied by the cosine and sine of the angle)
+			// and create a temp target (t) position of the x, y coordinates, for smoother movements
+			this.t = {
+				x: target.x - 0.8 * this.dt * Math.cos(this.angle),
+				y: target.y - 0.8 * this.dt * Math.sin(this.angle),
+			};
+
+			// recalculate the position of each segment, starting with the last (this.n -1)
+			if (this.t.x) {
+				this.segments[this.n - 1].update(this.t);
+			} else {
+				this.segments[this.n - 1].update(target);
+			}
+			// then iterate thru to the first segment starting with the next (2nd to last) one
+			for (let i = this.n - 2; i >= 0; i--) {
+				this.segments[i].update(this.segments[i + 1].pos);
+			}
+			/* Check to see if this object is close to / has reached the target position:
+            // if the distance between the object's current position and the target position
+            // are less than or equal to the sum of its (length + the distance between the 
+            // last target and the current target), then the fallback() method is called
+            // to ensure a more seamless transition by aligning the object's segments with
+            // its current position.
+            */
+			if (
+				dist(this.x, this.y, target.x, target.y) <=
+				this.l + dist(last_target.x, last_target.y, target.x, target.y)
+			) {
+				this.segments[0].fallback({ x: this.x, y: this.y });
+				for (let i = 1; i < this.n; i++) {
+					this.segments[i].fallback(this.segments[i - 1].nextPos);
+				}
+			}
+		} // close of move()
+		show(target) {
+			if (dist(this.x, this.y, target.x, target.y) <= this.l) {
+				c.globalCompositeOperation = 'lighter';
+				c.beginPath();
+				c.lineTo(this.x, this.y);
+				for (let i = 0; i < this.n; i++) {
+					this.segments[i].show();
+				}
+				c.strokeStyle =
+					'hsl(' +
+					(this.rand * 60 + 180) +
+					',100%,' +
+					(this.rand * 60 + 25) +
+					'%)';
+				c.lineWidth = this.rand * 2;
+				c.lineCap = 'round';
+				c.lineJoin = 'round';
+				c.stroke();
+				c.globalCompositeOperation = 'source-over';
+			}
+		} // close of show()
+		show2(target) {
+			c.beginPath();
+			if (dist(this.x, this.y, target.x, target.y) <= this.l) {
+				c.arc(this.x, this.y, 2 * this.rand + 1, 0, 2 * Math.PI);
+				c.fillStyle = 'white';
+			} else {
+				c.arc(this.x, this.y, this.rand * 2, 0, 2 * Math.PI);
+				c.fillStyle = 'darkcyan';
+			}
+			c.fill();
+		} // close of show2()
+	} // close of crawler class
+
+	class slowCrawler extends crawler {
+		constructor(x, y, l, n, a) {
+			super(x, y, l, n, a);
+		}
+
+		move(last_target, target) {
+			this.angle = Math.atan2(target.y - this.y, target.x - this.x);
+			this.dt = dist(last_target.x, last_target.y, target.x, target.y) + 5;
+
+			// divide this.dt (the delta time of the slowCrawler) by 4 to slow down the movement
+			this.dt /= 4;
+
+			this.t = {
+				x: target.x - 0.8 * this.dt * Math.cos(this.angle),
+				y: target.y - 0.8 * this.dt * Math.sin(this.angle),
+			};
+
+			if (this.t.x) {
+				this.segments[this.n - 1].update(this.t);
+			} else {
+				this.segments[this.n - 1].update(target);
+			}
+
+			for (let i = this.n - 2; i >= 0; i--) {
+				this.segments[i].update(this.segments[i + 1].pos);
+			}
+
+			if (
+				dist(this.x, this.y, target.x, target.y) <=
+				this.l + dist(last_target.x, last_target.y, target.x, target.y)
+			) {
+				this.segments[0].fallback({ x: this.x, y: this.y });
+				for (let i = 1; i < this.n; i++) {
+					this.segments[i].fallback(this.segments[i - 1].nextPos);
+				}
+			}
+		}
+	}
+
+	let maxl = 300,
+		minl = 50,
+		n = 33,
+		numt = 500,
+		tent = [],
+		clicked = false,
+		target = { x: 0, y: 0 },
+		last_target = {},
+		t = 0,
+		q = 10;
+
+	for (let i = 0; i < numt; i++) {
+		if (Math.random() > 0.5) {
+			tent.push(
+				new crawler(
+					Math.random() * w,
+					Math.random() * h,
+					Math.random() * (maxl - minl) + minl,
+					n,
+					Math.random() * 2 * Math.PI
+				)
+			);
+		} else {
+			tent.push(
+				new slowCrawler(
+					Math.random() * w,
+					Math.random() * h,
+					Math.random() * (maxl - minl) + minl,
+					n,
+					Math.random() * 2 * Math.PI
+				)
+			);
+		}
+	}
+
+	function draw() {
+		if (mouse.x) {
+			target.errx = mouse.x - target.x;
+			target.erry = mouse.y - target.y;
+		} else {
+			target.errx =
+				w / 2 +
+				((h / 2 - q) *
+					Math.sqrt(2) *
+					Math.cos((t / 1000) * 2 * Math.PI) *
+					Math.sin((t / 1000) * 2 * Math.PI)) /
+					(Math.pow(Math.sin((t / 1000) * 2 * Math.PI), 2) + 1) -
+				target.x;
+			target.erry =
+				h / 2 +
+				((h / 2 - q) *
+					Math.sqrt(2) *
+					Math.cos((t / 1000) * 2 * Math.PI) *
+					Math.sin((t / 1000) * 2 * Math.PI)) /
+					(Math.pow(Math.sin((t / 1000) * 2 * Math.PI), 2) + 1) -
+				target.y;
+		}
+
+		target.x += target.errx / 10;
+		target.y += target.erry / 10;
+
+		t += 0.01;
+
+		c.beginPath();
+		c.arc(
+			target.x,
+			target.y,
+			dist(last_target.x, last_target.y, target.x, target.y) + 5,
+			0,
+			2 * Math.PI
+		);
+		this.ctx.fillStyle = 'hsla(' + this.hue + ', 100%, 50%, 0.1)';
+
+		c.fill();
+
+        for (let i = 0; i < numt; i++) {
+            tent[i].move(last_target, target);
+            // check if the crawler is an instance of slowCrawler
+            if (tent[i] instanceof slowCrawler) {
+                tent[i].show2(target); // use a different show method for slowCrawlers
+            } else {
+                tent[i].show(target); // use normal show method for regular crawlers
+            }
         }
-        if (this.y > canvas.height || this.y < 0) {
-            this.directionY = -this.directionY;
+
+        last_target.x = target.x;
+        last_target.y = target.y;
+    } // end of draw() function
+
+    // continuously call the draw() function
+    function loop() {
+        c.fillStyle = 'rgba(30,30,30,1)';
+        c.fillRect(0, 0, w, h);
+        draw();
+        window.requestAnimFrame(loop);
+    }
+
+    // event listener for when the user clicks
+    canvas.addEventListener('mousedown', function (e) {
+        clicked = true;
+        last_mouse.x = e.pageX - this.offsetLeft;
+        last_mouse.y = e.pageY - this.offsetTop;
+    });
+
+    // event listener for when the user releases the mouse click
+    canvas.addEventListener('mouseup', function () {
+        clicked = false;
+        last_mouse.x = false;
+        last_mouse.y = false;
+    });
+
+    // event listener for when the user moves the mouse
+    canvas.addEventListener('mousemove', function (e) {
+        if (clicked) {
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop;
         }
+    });
 
-        // check collision detection - mouse position / particle position
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < mouse.radius + this.size) {
-            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
-                this.x += 10;
-            }
-            if (mouse.x > this.x && this.x > this.size * 10) {
-                this.x -= 10;
-            }
-            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-                this.y += 10;
-            }
-            if (mouse.y > this.y && this.y > this.size * 10) {
-                this.y -= 10;
-            }
-        }
-        // move particle
-        this.x += this.directionX;
-        this.y += this.directionY;
-        // draw particle
-        this.draw();
-    }
-}
-
-// create particle array
-function init() {
-    particlesArray = [];
-    let numberOfParticles = (canvas.height * canvas.width) / 9000;
-    for (let i = 0; i < numberOfParticles; i++) {
-        let size = (Math.random() * 5) + 1;
-        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
-        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
-        let directionX = (Math.random() * 5) - 2.5;
-        let directionY = (Math.random() * 5) - 2.5;
-        let color = 'rgba(0, 255, 0, 0.5)'; // neon green color
-
-        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
-    }
-}
-
-// also updated this function to connectParticles() here in v3
-function connectParticles() {
-    let opacityValue = 1;
-    for (let a = 0; a < particlesArray.length; a++) {
-        for (let b = a; b < particlesArray.length; b++) {
-            let distance = (( particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
-                         + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
-            if (distance < (canvas.width/7) * (canvas.height/7)) {
-                opacityValue = 1 - (distance/20000);
-                ctx.strokeStyle='rgba(157, 192, 249, ' + opacityValue + ')'; // revert to original blue color for lines
-                ctx.lineWidth = 1;
-                ctx.beginPath();
-                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
-                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
-                ctx.stroke();
-            }
-        }
-    }
-}
-
-// animation loop
-function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
-    }
-    connectParticles();
-}
-
-// resize event
-window.addEventListener('resize', function() {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    mouse.radius = ((canvas.height/80) * (canvas.width/80));
-    init();
-});
-
-// mouse out event
-window.addEventListener('mouseout', function() {
-    mouse.x = undefined;
-    mouse.y = undefined;
-});
-
-window.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-});
-
-init();
-animate();
+    loop(); // call the loop() function to start the animation
+}; // end of window.onload function
